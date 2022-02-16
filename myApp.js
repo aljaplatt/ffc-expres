e36e11fa1779a0e03fefb7ad55cc7fcbb9a5a743
Alex Platt <alja.platt@gmail.com>
2022-02-16 18:29:20 +0000

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,66 +1,15 @@
 var express = require("express");
-var dotenv = require("dotenv").config();
 var app = express();
+console.log("Hello World");
 
-/**
- * You can respond to requests with a file using the res.sendFile(path) method. You can put it inside the app.get('/', ...) route handler. Behind the scenes, this method will set the appropriate headers to instruct your browser on how to handle the file you want to send, according to its type. Then it will read and send the file. This method needs an absolute file path. We recommend you to use the Node global variable __dirname to calculate the path like this:
-
-absolutePath = __dirname + relativePath/file.ext
-
-Send the /views/index.html file as a response to GET requests to the / path. If you view your live app, you should see a big HTML heading (and a form that we will use later…), with no style applied.
- */
-
-/**
- * An HTML server usually has one or more directories that are accessible by the user. You can place there the static assets needed by your application (stylesheets, scripts, images).
-
-In Express, you can put in place this functionality using the middleware express.static(path), where the path parameter is the absolute path of the folder containing the assets.
-
-If you don’t know what middleware is... don’t worry, we will discuss in detail later. Basically, middleware are functions that intercept route handlers, adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction). The first path argument is optional. If you don’t pass it, the middleware will be executed for all requests.
-
-Mount the express.static() middleware to the path /public with app.use(). The absolute path to the assets folder is __dirname + /public.
-
-Now your app should be able to serve a CSS stylesheet. Note that the /public/style.css file is referenced in the /views/index.html in the project boilerplate. Your front-page should look a little better now!
- */
-
-app.use("/public", express.static(__dirname + "/public"));
-
-app.get("/", (req, res) => {
-  // res.send('Hello Express')
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
+  app.use(express.static(__dirname + "/public"));
 });
-
-const message = "Hello json";
-app.get("/json", (req, res) =>
-  res.json({
-    message:
-      process.env.MESSAGE_STYLE === "uppercase"
-        ? message.toUpperCase()
-        : message,
-  })
-);
-
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  }
+  res.json({ message: "Hello json" });
+});
 module.exports = app;
-
-/**
- * Let's create a simple API by creating a route that responds with JSON at the path /json. You can do it as usual, with the app.get() method. Inside the route handler, use the method res.json(), passing in an object as an argument. This method closes the request-response loop, returning the data. Behind the scenes, it converts a valid JavaScript object into a string, then sets the appropriate headers to tell your browser that you are serving JSON, and sends the data back. A valid object has the usual structure {key: data}. data can be a number, a string, a nested object or an array. data can also be a variable or the result of a function call, in which case it will be evaluated before being converted into a string.
- *
- * *
- */
-// let message = { message: "Hello json" };
-
-// app.get("/json", (req, res) => {
-//   // const message =
-//   //   process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
-//   // let message = "Hello World".toUpperCase();
-//   if (process.env.MESSAGE_STYLE === "uppercase") {
-//     return res.json({ message: "HELLO JSON" });
-//   } else {
-//     return res.json(message);
-//   }
-// });
-
-/**
- * The .env file is a hidden file that is used to pass environment variables to your application. This file is secret, no one but you can access it, and it can be used to store data that you want to keep private or hidden. For example, you can store API keys from external services or your database URI. You can also use it to store configuration options. By setting configuration options, you can change the behavior of your application, without the need to rewrite some code.
-
-The environment variables are accessible from the app as process.env.VAR_NAME. The process.env object is a global Node object, and variables are passed as strings. By convention, the variable names are all uppercase, with words separated by an underscore. The .env is a shell file, so you don’t need to wrap names or values in quotes. It is also important to note that there cannot be space around the equals sign when you are assigning values to your variables, e.g. VAR_NAME=value. Usually, you will put each variable definition on a separate line.
- */
