02e1f1bf3ee1897a2ce45a8319d4e4c1c0eaeceb
Alex Platt <alja.platt@gmail.com>
2022-02-16 17:23:36 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -9,6 +9,18 @@ absolutePath = __dirname + relativePath/file.ext
 Send the /views/index.html file as a response to GET requests to the / path. If you view your live app, you should see a big HTML heading (and a form that we will use later…), with no style applied.
  */
 
+/**
+ * An HTML server usually has one or more directories that are accessible by the user. You can place there the static assets needed by your application (stylesheets, scripts, images).
+
+In Express, you can put in place this functionality using the middleware express.static(path), where the path parameter is the absolute path of the folder containing the assets.
+
+If you don’t know what middleware is... don’t worry, we will discuss in detail later. Basically, middleware are functions that intercept route handlers, adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction). The first path argument is optional. If you don’t pass it, the middleware will be executed for all requests.
+
+Mount the express.static() middleware to the path /public with app.use(). The absolute path to the assets folder is __dirname + /public.
+
+Now your app should be able to serve a CSS stylesheet. Note that the /public/style.css file is referenced in the /views/index.html in the project boilerplate. Your front-page should look a little better now!
+ */
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
