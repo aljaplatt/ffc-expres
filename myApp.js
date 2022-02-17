430c313223fd970a8b8fe28eaa065bf38e440fa2
Alex Platt <alja.platt@gmail.com>
2022-02-17 08:30:31 +0000

    middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 var express = require("express");
+
 var app = express();
-console.log("Hello World");
+// console.log("Hello World");
+
+app.use((req, res, next) => {
+  const { method, path } = req;
+  const ip = "127.0.0.1";
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
@@ -13,3 +21,12 @@ app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
 });
 module.exports = app;
+
+/**
+ * Earlier, you were introduced to the express.static() middleware function. Now it’s time to see what middleware is, in more detail. Middleware functions are functions that take 3 arguments: the request object, the response object, and the next function in the application’s request-response cycle. These functions execute some code that can have side effects on the app, and usually add information to the request or response objects. They can also end the cycle by sending a response when some condition is met. If they don’t send the response when they are done, they start the execution of the next function in the stack. This triggers calling the 3rd argument, next().
+ * 
+ * Let’s suppose you mounted this function on a route. When a request matches the route, it displays the string “I’m a middleware…”, then it executes the next function in the stack. In this exercise, you are going to build root-level middleware. As you have seen in challenge 4, to mount a middleware function at root level, you can use the app.use(<mware-function>) method. In this case, the function will be executed for all the requests, but you can also set more specific conditions. For example, if you want a function to be executed only for POST requests, you could use app.post(<mware-function>). Analogous methods exist for all the HTTP verbs (GET, DELETE, PUT, …).
+
+Build a simple logger. For every request, it should log to the console a string taking the following format: method path - ip. An example would look like this: GET /json - ::ffff:127.0.0.1. Note that there is a space between method and path and that the dash separating path and ip is surrounded by a space on both sides. You can get the request method (http verb), the relative route path, and the caller’s ip from the request object using req.method, req.path and req.ip. Remember to call next() when you are done, or your server will be stuck forever. Be sure to have the ‘Logs’ opened, and see what happens when some request arrives.
+ * 
+ */
